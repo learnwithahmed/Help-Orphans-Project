0a622d1cc8f19ad4a4082f417dcf78f6593c8107
Tahseen Alaa <raspoteen7@gmail.com>
2018-06-01 13:07:06 +0300

    Fix Bug.

--- config/database.php
+++ config/database.php
@@ -35,7 +35,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'orphans_db'),
             'username' => env('DB_USERNAME', 'user1'),
